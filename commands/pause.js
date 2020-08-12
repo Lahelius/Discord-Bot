36ddef06bcb20b16ff6e259e29e4d7026fbd8616
Gotanks <34191122+Gotanks@users.noreply.github.com>
2020-08-12 14:23:11 -0400

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -6,8 +6,8 @@ module.exports = {
     description : 'Pauses the currently playing track',
     async execute(message, args, queue){
         const serverQueue = queue.get(message.guild.id)
-        if(!message.member.voice.channel) return message.channel.send('You to be in a voice channel to use the pause command.')
-        if(!serverQueue) return message.channel.send('There is not a song that is currently playing')
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use the pause command.')
+        if(!serverQueue) return message.channel.send('No song is currently being played')
         if(!serverQueue.playing) return message.channel.send("The current track is already paused")
         serverQueue.playing = false
         serverQueue.connection.dispatcher.pause()
