440461ea74f600371182305e254bdc902599b8e1
Brandon P. Lahey <blahey1997@gmail.com>
2020-08-11 22:31:38 -0400

    Play now supports a basic queue

--- commands/play.js
+++ commands/play.js
@@ -2,27 +2,65 @@ const ytdl = require('ytdl-core')
 module.exports = {
   name: 'play',
   descrpition: "Loads and plays the song",
-  async execute(message, args){
+  async execute(message, args, serverQueue, queue){
     const voiceChannel = message.member.voice.channel
     if(!voiceChannel) return message.channel.send("You need to be in a voice channel to play music")
     const permissions = voiceChannel.permissionsFor(message.client.user)
     if(!permissions.has('CONNECT')) return message.channel.send("I dont\'t have permission to connect to the voice channel!")
     if(!permissions.has('SPEAK')) return message.channel.send("I dont\'t have permission to speak in the voice channel!")
 
-    try{
-      var connection = await voiceChannel.join()
-    } catch(error){
-      console.log(`There was an error connecting to the voice channel: ${error}`)
-      return message.channel.send(`There was an error connecting to the voice channel: ${error}`)
+    const songDetails = await ytdl.getInfo(args[1])
+    const song = {
+      title: songDetails.videoDetails.title,
+      url: songDetails.videoDetails.video_url
     }
-
-    const dispatcher = connection.play(ytdl(args[1]))
-    .on('finish', () => {
-      voiceChannel.leave()
-    })
-    .on('error', error =>{
-      console.log(error)
-    })
-    dispatcher.setVolumeLogarithmic(5/5)
+    
+    if(!serverQueue){
+      const queueConstruct = {
+        textChannel : message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      }
+      queue.set(message.guild.id, queueConstruct)
+      queueConstruct.songs.push(song)
+    
+      try{
+        var connection = await voiceChannel.join()
+        queueConstruct.connection = connection
+        playSong(message.guild, queueConstruct.songs[0], queue)
+      } catch(error){
+        console.log(`There was an error connecting to the voice channel: ${error}`)
+        return message.channel.send(`There was an error connecting to the voice channel: ${error}`)
+      }
+    } else{
+      serverQueue.songs.push(song)
+      return message.channel.send(`${song.title} has been added to the queue`)
+    }
   }
+}
+
+
+function playSong(guild, song, queue){
+  const serverQueue = queue.get(guild.id)
+  
+  if(!song){
+    serverQueue.voiceChannel.leave()
+    queue.delete(guild.id)
+    return
+  }
+
+  const dispatcher = serverQueue.connection.play(ytdl(song.url))
+  
+  .on('finish', () => {
+    serverQueue.songs.shift()
+    playSong(guild, serverQueue.songs[0], queue)
+  })
+  .on('error', error =>{
+    console.log(error)
+  })
+  dispatcher.setVolumeLogarithmic(serverQueue.volume/5)
+
 }
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const {PREFIX, token} = require('./config.json')
 const client = new Discord.Client()
 const ytdl =  require('ytdl-core')
+const queue = new Map()
+
 
 client.commands = new Discord.Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
@@ -18,9 +20,10 @@ client.on('message', async message => {
   if(!message.content.startsWith(PREFIX)) return
 
   const args = message.content.substring(PREFIX.length).split(" ")
+  const serverQueue = queue.get(message.guild.id)
 
   if(message.content.startsWith(`${PREFIX}play`)){
-    client.commands.get('play').execute(message, args)
+    client.commands.get('play').execute(message, args, serverQueue, queue)
   } else if (message.content.startsWith(`${PREFIX}leave`)){
     client.commands.get('leave').execute(message, args)
   } else if (message.content.startsWith(`${PREFIX}join`)) {
