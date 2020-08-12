4f69a4e3003db9414076bdf0cfe01510e6c82e50
Gotanks <34191122+Gotanks@users.noreply.github.com>
2020-08-12 14:21:53 -0400

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,13 +3,12 @@ module.exports = {
     description : 'Resumes the currently playing track',
     async execute(message, args, queue){
         const serverQueue = queue.get(message.guild.id)
-        if(!message.member.voice.channel) return message.channel.send('You to be in a voice channel to use the resume command.')
-        if(!serverQueue) return message.channel.send('There is not a song that is currently playing')
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use the resume command.')
+        if(!serverQueue) return message.channel.send('No song is currently being played')
         if(serverQueue.playing) return message.channel.send("The music is already playing")
         serverQueue.playing = true
         serverQueue.connection.dispatcher.resume()
         message.channel.send('The music has been resumed.')
         return undefined
     }
-
 }
