dab1f672d60dde44ac9b7c330eb0de3dfca5e069
Gotanks <34191122+Gotanks@users.noreply.github.com>
2020-08-30 22:51:41 -0400

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,1 +1,10 @@
 
+module.exports = {
+    name: 'Shuffle',
+    description: 'Shuffles the songs in the playlist or Queue',
+    async execute(message, args, queue){
+        const serverQueue = queue.get(message.guild.id)
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use music commands')
+        if(!serverQueue) return message.channel.send("No song is currently playing")
+    }
+}
